d2b9c1555de036c4271ee63c669671cd2e8a8f14
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-03-05 17:06:05 +0000

    Update realdebrid.js

--- src/lib/debrid/realdebrid.js
+++ src/lib/debrid/realdebrid.js
@@ -1,20 +1,19 @@
-import {createHash} from 'crypto';
-import {ERROR} from './const.js';
-import {wait, isVideo} from '../util.js';
+const { createHash } = require('crypto');
+const { ERROR } = require('./const.js');
+const { wait, isVideo } = require('../util.js');
 
-export default class RealDebrid {
-
+class RealDebrid {
   static id = 'realdebrid';
   static name = 'Real-Debrid';
   static shortName = 'RD';
   static configFields = [
     {
-      type: 'text', 
-      name: 'debridApiKey', 
-      label: `Real-Debrid API Key`, 
-      required: true, 
-      href: {value: 'https://real-debrid.com/apitoken', label:'Get API Key Here'}
-    }
+      type: 'text',
+      name: 'debridApiKey',
+      label: `Real-Debrid API Key`,
+      required: true,
+      href: { value: 'https://real-debrid.com/apitoken', label: 'Get API Key Here' },
+    },
   ];
 
   #apiKey;
@@ -26,144 +25,135 @@ export default class RealDebrid {
     this.#ip = userConfig.ip || '';
   }
 
-  async getTorrentsCached(torrents, isValidCachedFiles){
-    const hashList = torrents.map(torrent => torrent.infos.infoHash).filter(Boolean);
+  async getTorrentsCached(torrents, isValidCachedFiles) {
+    const hashList = torrents.map((torrent) => torrent.infos.infoHash).filter(Boolean);
     const res = await this.#request('GET', `/torrents/instantAvailability/${hashList.join('/')}`);
-    return torrents.filter(torrent => {
+    return torrents.filter((torrent) => {
       const cachedFiles = [];
       const caches = (res[torrent.infos.infoHash]?.rd || []).filter(this.#isVideoCache);
-      for(const cache of caches){
-        for(const file of Object.values(cache)){
-          const f = {name: file.filename, size: file.filesize};
-          if(!cachedFiles.includes(f))cachedFiles.push(f);
+      for (const cache of caches) {
+        for (const file of Object.values(cache)) {
+          const f = { name: file.filename, size: file.filesize };
+          if (!cachedFiles.includes(f)) cachedFiles.push(f);
         }
       }
       return cachedFiles.length > 0 && isValidCachedFiles(cachedFiles);
     });
   }
 
-  async getProgressTorrents(torrents){
+  async getProgressTorrents(torrents) {
     const res = await this.#request('GET', '/torrents');
     return res.reduce((progress, torrent) => {
       progress[torrent.hash] = {
         percent: torrent.progress || 0,
-        speed: torrent.speed || 0
-      }
+        speed: torrent.speed || 0,
+      };
       return progress;
     }, {});
   }
 
-  async getFilesFromMagnet(magnet, infoHash){
+  async getFilesFromMagnet(magnet, infoHash) {
     const torrentId = await this.#searchTorrentIdByHash(infoHash);
-    if(torrentId)return this.#getFilesFromTorrent(torrentId);
+    if (torrentId) return this.#getFilesFromTorrent(torrentId);
     const body = new FormData();
     body.append('magnet', magnet);
-    const res = await this.#request('POST', `/torrents/addMagnet`, {body});
+    const res = await this.#request('POST', `/torrents/addMagnet`, { body });
     return this.#getFilesFromTorrent(res.id);
   }
 
-  async getFilesFromBuffer(buffer, infoHash){
+  async getFilesFromBuffer(buffer, infoHash) {
     const torrentId = await this.#searchTorrentIdByHash(infoHash);
-    if(torrentId)return this.#getFilesFromTorrent(torrentId);
+    if (torrentId) return this.#getFilesFromTorrent(torrentId);
     const body = buffer;
-    const res = await this.#request('PUT', `/torrents/addTorrent`, {body});
+    const res = await this.#request('PUT', `/torrents/addTorrent`, { body });
     return this.#getFilesFromTorrent(res.id);
   }
 
-  async getDownload(file){
-
+  async getDownload(file) {
     const [torrentId, fileId] = file.id.split(':');
 
     let torrent = await this.#request('GET', `/torrents/info/${torrentId}`);
     let body;
 
-    if(torrent.status == 'waiting_files_selection'){
-
+    if (torrent.status == 'waiting_files_selection') {
       const caches = await this.#request('GET', `/torrents/instantAvailability/${torrent.hash}`);
       const bestCache = (caches[torrent.hash]?.rd || [])
-        .filter(cache => cache[fileId] && this.#isVideoCache(cache))
+        .filter((cache) => cache[fileId] && this.#isVideoCache(cache))
         .sort((a, b) => Object.values(b).length - Object.values(a).length)
         .shift();
 
-      const fileIds = bestCache ? Object.keys(bestCache) : torrent.files.filter(file => isVideo(file.path)).map(file => file.id);
+      const fileIds = bestCache
+        ? Object.keys(bestCache)
+        : torrent.files.filter((file) => isVideo(file.path)).map((file) => file.id);
       body = new FormData();
       body.append('files', fileIds.join(','));
 
-      await this.#request('POST', `/torrents/selectFiles/${torrentId}`, {body});
+      await this.#request('POST', `/torrents/selectFiles/${torrentId}`, { body });
       torrent = await this.#request('GET', `/torrents/info/${torrentId}`);
-
     }
 
-    if(torrent.status != 'downloaded'){
+    if (torrent.status != 'downloaded') {
       throw new Error(ERROR.NOT_READY);
     }
 
-    const linkIndex = torrent.files.filter(file => file.selected).findIndex(file => file.id == fileId);
+    const linkIndex = torrent.files.filter((file) => file.selected).findIndex((file) => file.id == fileId);
     const link = torrent.links[linkIndex] || false;
 
-    if(!link){
+    if (!link) {
       throw new Error(`LinkIndex or link not found`);
     }
 
     body = new FormData();
     body.append('link', link);
-    const res = await this.#request('POST', '/unrestrict/link', {body});
+    const res = await this.#request('POST', '/unrestrict/link', { body });
     return res.download;
-
   }
 
-  async getUserHash(){
+  async getUserHash() {
     return createHash('md5').update(this.#apiKey).digest('hex');
   }
 
-  // Return false when a non video file is available in the cache to avoid rar files
-  async #isVideoCache(cache){
-    return !Object.values(cache).find(file => !isVideo(file.filename));
+  // Return false when a non-video file is available in the cache to avoid rar files
+  async #isVideoCache(cache) {
+    return !Object.values(cache).find((file) => !isVideo(file.filename));
   }
 
-  async #getFilesFromTorrent(id){
-
+  async #getFilesFromTorrent(id) {
     let torrent = await this.#request('GET', `/torrents/info/${id}`);
-
     return torrent.files.map((file, index) => {
       return {
         name: file.path.split('/').pop(),
         size: file.bytes,
         id: `${torrent.id}:${file.id}`,
         url: '',
-        ready: null
+        ready: null,
       };
     });
-
   }
 
-  async #searchTorrentIdByHash(hash){
-
+  async #searchTorrentIdByHash(hash) {
     const torrents = await this.#request('GET', `/torrents`);
-
-    for(let torrent of torrents){
-      if(torrent.hash == hash && ['magnet_conversion', 'waiting_files_selection', 'queued', 'downloading', 'downloaded'].includes(torrent.status)){
+    for (let torrent of torrents) {
+      if (torrent.hash == hash && ['magnet_conversion', 'waiting_files_selection', 'queued', 'downloading', 'downloaded'].includes(torrent.status)) {
         return torrent.id;
       }
     }
-
   }
 
-  async #request(method, path, opts){
-
+  async #request(method, path, opts) {
     opts = opts || {};
     opts = Object.assign(opts, {
       method,
       headers: Object.assign(opts.headers || {}, {
         'accept': 'application/json',
-        'authorization': `Bearer ${this.#apiKey}`
+        'authorization': `Bearer ${this.#apiKey}`,
       }),
-      query: opts.query || {}
+      query: opts.query || {},
     });
 
-    if(method == 'POST' || method == 'PUT'){
+    if (method == 'POST' || method == 'PUT') {
       opts.body = opts.body || new FormData();
-      if(this.#ip && opts.body instanceof FormData)opts.body.append('ip', this.#ip);
+      if (this.#ip && opts.body instanceof FormData) opts.body.append('ip', this.#ip);
     }
 
     const url = `https://api.real-debrid.com/rest/1.0${path}?${new URLSearchParams(opts.query).toString()}`;
@@ -172,12 +162,12 @@ export default class RealDebrid {
 
     try {
       data = await res.json();
-    }catch(err){
-      data = res.status >= 400 ? {error_code: -2, error: `Empty response ${res.status}`} : {};
+    } catch (err) {
+      data = res.status >= 400 ? { error_code: -2, error: `Empty response ${res.status}` } : {};
     }
 
-    if(data.error_code){
-      switch(data.error_code){
+    if (data.error_code) {
+      switch (data.error_code) {
         case 8:
           throw new Error(ERROR.EXPIRED_API_KEY);
         case 20:
@@ -188,7 +178,7 @@ export default class RealDebrid {
     }
 
     return data;
-
   }
-
 }
+
+module.exports = RealDebrid;
