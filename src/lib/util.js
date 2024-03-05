f0a45efadc05771699074d8a72a7c4054fa9bb81
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-03-05 16:59:16 +0000

    Update util.js

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,56 +1,64 @@
-import { setTimeout } from 'timers/promises';
+export function numberPad(number, count = 2) {
+  return `${number}`.padStart(count, 0);
+}
 
-export const numberPad = (number, count = 2) => `${number}`.padStart(count, 0);
+export function parseWords(str) {
+  return str.replace(/[^a-zA-Z0-9]+/g, ' ').split(' ').filter(Boolean);
+}
 
-export const parseWords = (str) => str.replace(/[^a-zA-Z0-9]+/g, ' ').split(' ').filter(Boolean);
+export function sortBy(...keys) {
+  return (a, b) => {
+    if (typeof keys[0] === 'string') keys = [keys];
+    for (const [key, reverse] of keys) {
+      if (a[key] > b[key]) return reverse ? -1 : 1;
+      if (a[key] < b[key]) return reverse ? 1 : -1;
+    }
+    return 0;
+  };
+}
 
-export const sortBy = (...keys) => (a, b) => {
-  if (typeof keys[0] === 'string') keys = [keys];
-  for (const [key, reverse] of keys) {
-    if (a[key] > b[key]) return reverse ? -1 : 1;
-    if (a[key] < b[key]) return reverse ? 1 : -1;
-  }
-  return 0;
-};
-
-export const bytesToSize = (bytes) => {
+export function bytesToSize(bytes) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   if (bytes === 0) return '0 Byte';
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
   return `${Math.round(bytes / Math.pow(1024, i) * 100) / 100} ${sizes[i]}`;
-};
+}
 
-export const wait = (ms) => setTimeout(ms);
+export function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-export const isVideo = (filename) => [
-  '3g2',
-  '3gp',
-  'avi',
-  'flv',
-  'mkv',
-  'mk3d',
-  'mov',
-  'mp2',
-  'mp4',
-  'm4v',
-  'mpe',
-  'mpeg',
-  'mpg',
-  'mpv',
-  'webm',
-  'wmv',
-  'ogm',
-  'ts',
-  'm2ts',
-].includes(filename?.split('.').pop());
+export function isVideo(filename) {
+  return [
+    '3g2',
+    '3gp',
+    'avi',
+    'flv',
+    'mkv',
+    'mk3d',
+    'mov',
+    'mp2',
+    'mp4',
+    'm4v',
+    'mpe',
+    'mpeg',
+    'mpg',
+    'mpv',
+    'webm',
+    'wmv',
+    'ogm',
+    'ts',
+    'm2ts',
+  ].includes(filename?.split('.').pop());
+}
 
-export const promiseTimeout = (promise, ms) => {
+export async function promiseTimeout(promise, ms) {
   const ac = new AbortController();
-  const waitPromise = setTimeout(ms, null, { signal: ac.signal }).then(() =>
-    Promise.reject(`Max execution time reached ${ms}`)
+  const waitPromise = new Promise((_, reject) =>
+    setTimeout(() => reject(`Max execution time reached ${ms}`), ms)
   );
   return Promise.race([waitPromise, promise.then((res) => {
     ac.abort();
     return res;
   })]);
-};
+}
