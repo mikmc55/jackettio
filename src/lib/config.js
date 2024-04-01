9e9279daca59d86ca609510af806acc77bd6643a
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-04-01 17:29:14 +0100

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,9 +4,9 @@ module.exports = {
   // https://expressjs.com/en/guide/behind-proxies.html
   trustProxy: boolOrString(process.env.TRUST_PROXY || 'loopback, linklocal, uniquelocal'),
   // Jacket instance url
-  jackettUrl: process.env.JACKETT_URL || 'http://98.97.70.217:9117',
+  jackettUrl: process.env.JACKETT_URL || 'http://195.154.231.70:9117',
   // Jacket API key
-  jackettApiKey: process.env.JACKETT_API_KEY || 'dkkgc3sfj5j7d6zs51jbuzxab6vqy00u',
+  jackettApiKey: process.env.JACKETT_API_KEY || 'ar2u3emdwocp9cqcbyiugot0t17s65yl',
   // Data folder for cache database, torrent files ... Must be persistent in production
   dataFolder: process.env.DATA_FOLDER || '/tmp',
   // Enable localtunnel feature
